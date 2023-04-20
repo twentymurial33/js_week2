ee1b0daabbcf934544639ddcef528c02ba9ae142
Murial Anindo <murial.anindo@lonett.com>
2023-04-20 14:58:10 -0400

    started working on js assigment

---
+++ conditionals/index.js
@@ -0,0 +1,23 @@
+//Write a JavaScript program that accepts two integers and displays the larger of the two.
+
+function twoCalculations(a, b) {
+  if (condition) {
+  } else {
+  }
+}
+
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+
+//Write a JavaScript program that checks whether an integer is an even or an odd number.
+
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
